e14b44854d5bb3d5965f7ffe57e143c41c0ce8e0
Bas <bas@muer.nl>
2020-12-03 09:16:04 +0100

    Day three

---
+++ day-three/trees.php
@@ -0,0 +1,26 @@
+<?php
+
+$map = require 'input.php';
+
+function travel(array $map, int $stepLeft, int $stepDown)
+{
+    $trees = 0;
+    $charsInLine = strlen($map[0]);
+    $numLines = count($map);
+    for ($left = $stepLeft, $down = $stepDown; $down < $numLines; $left += $stepLeft, $down += $stepDown) {
+        if ('#' === substr($map[$down], $left % $charsInLine, 1)) {
+            $trees++;
+        }
+    }
+
+    return $trees;
+}
+
+$total = 1;
+foreach ([[1,1],[3,1],[5,1],[7,1],[1,2]] as $steps) {
+    $result = travel($map, $steps[0], $steps[1]);
+    $total *= $result;
+    printf("Left: %d, down: %d, result: %d trees, total %d\n", $steps[0], $steps[1], $result, $total);
+}
+
+print $total . "\n\n";
