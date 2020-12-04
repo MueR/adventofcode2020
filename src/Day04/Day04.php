6a68abd3c76c785a2bf362ad85fff2c201ae29ec
Bas <bas@muer.nl>
2020-12-04 13:34:52 +0100

    Minor improvement because I can..

--- src/Day04/Day04.php
+++ src/Day04/Day04.php
@@ -8,32 +8,17 @@ use MueR\AdventOfCode2020\AbstractSolver;
 class Day04 extends AbstractSolver
 {
     private array $passports = [];
+    private array $requiredFields;
+    private int $numberOfTests;
 
-    #[Pure] public function partOne(): int
+    public function __construct()
     {
-        $requiredFields = [
-            'byr', 'iyr', 'eyr', 'hgt',
-            'hcl', 'ecl', 'pid',
-        ];
+        parent::__construct();
 
-        $valid = 0;
-        foreach ($this->passports as $index => $passport) {
-            $passKeys = \array_keys($passport);
-            $intersect = \array_intersect($requiredFields, $passKeys);
-            if (count($intersect) === count($requiredFields)) {
-                $valid++;
-            }
-        }
-
-        return $valid;
-    }
-
-    public function partTwo(): int
-    {
-        $requiredFields = [
+        $this->requiredFields = [
             'byr' => fn($test) => \preg_match('/^[\d]{4}$/', $test) && $this->numBetween((int)$test, 1920, 2002),
-            'iyr' => fn($test) => \preg_match('/^20(1[\d]|20)$/', $test) && $this->numBetween((int)$test, 2010, 2020),
-            'eyr' => fn($test) => \preg_match('/^20(2[\d]|30)$/', $test) && $this->numBetween((int)$test, 2020, 2030),
+            'iyr' => fn($test) => \preg_match('/^20(1[\d]|20)$/', $test),
+            'eyr' => fn($test) => \preg_match('/^20(2[\d]|30)$/', $test),
             'hgt' => function ($test) {
                 if (!\preg_match('/^(\d{2,3})(cm|in)$/', $test)) {
                     return false;
@@ -45,17 +30,30 @@ class Day04 extends AbstractSolver
             'ecl' => fn($test) => \preg_match('/^(amb|blu|brn|gry|grn|hzl|oth)$/', $test),
             'pid' => fn($test) => \preg_match('/^[\d]{9}$/', $test),
         ];
+        $this->numberOfTests = \count($this->requiredFields);
+    }
 
+    #[Pure] public function partOne(): int
+    {
+        $valid = 0;
         foreach ($this->passports as $index => $passport) {
-            foreach ($requiredFields as $field => $testFn) {
-                if (!\array_key_exists($field, $passport) || !$testFn($passport[$field])) {
-                    unset($this->passports[$index]);
-                    continue 2;
-                }
+            $passKeys = \array_keys($passport);
+            $intersect = \array_intersect(\array_keys($this->requiredFields), $passKeys);
+            if (count($intersect) === count(\array_keys($this->requiredFields))) {
+                $valid++;
             }
         }
 
-        return count($this->passports);
+        return $valid;
+    }
+
+    public function partTwo(): int
+    {
+        return \count(\array_filter($this->passports, function ($passport) {
+            return $this->numberOfTests === \count(\array_filter($this->requiredFields, static function ($testFn, $field) use ($passport) {
+                return \array_key_exists($field, $passport) && $testFn($passport[$field]);
+            }, \ARRAY_FILTER_USE_BOTH));
+        }));
     }
 
     protected function numBetween(int $val, int $min, int $max): bool
